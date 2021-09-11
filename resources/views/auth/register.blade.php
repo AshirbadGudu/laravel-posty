a0a359dcda0ee37aac26c79f7c422b45ff17b01c
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-09-11 14:53:05 +0530

    validation added in form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,28 +7,55 @@
             @csrf
             <div class="mb-4">
                 <label for="name" class="sr-only">Name</label>
-                <input type="text" id="name" name="name" placeholder="Enter your name"
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg">
+                <input value="{{old('name')}}" type="text" id="name" name="name" placeholder="Enter your name" class="bg-gray-100 border-2 w-full p-4 rounded-lg
+                    @error('name') border-red-500 @enderror
+                    ">
+                @error('name')
+                <div class="text-red-500 mt-2 text-small">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div class="mb-4">
                 <label for="username" class="sr-only">Username</label>
-                <input type="text" id="username" name="username" placeholder="Enter your username"
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg">
+                <input value="{{old('username')}}" type="text" id="username" name="username"
+                    placeholder="Enter your username" class="bg-gray-100 border-2 w-full p-4 rounded-lg
+                    @error('username') border-red-500 @enderror
+                    ">
+                @error('username')
+                <div class="text-red-500 mt-2 text-small">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div class="mb-4">
                 <label for="email" class="sr-only">Email</label>
-                <input type="email" id="email" name="email" placeholder="Enter your email"
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg">
+                <input value="{{old('email')}}" type="email" id="email" name="email" placeholder="Enter your email"
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg
+                    @error('email') border-red-500 @enderror
+                    ">
+                @error('email')
+                <div class="text-red-500 mt-2 text-small">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div class="mb-4">
                 <label for="password" class="sr-only">password</label>
-                <input type="password" id="password" name="password" placeholder="Enter your password"
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg">
+                <input type="password" id="password" name="password" placeholder="Enter your password" class="bg-gray-100 border-2 w-full p-4 rounded-lg
+                    @error('password') border-red-500 @enderror
+                    ">
+                @error('password')
+                <div class="text-red-500 mt-2 text-small">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div class="mb-4">
                 <label for="password_confirmation" class="sr-only">Retype Password</label>
-                <input type="password_confirmation" id="password_confirmation" name="password_confirmation"
+                <input type="password" id="password_confirmation" name="password_confirmation"
                     placeholder="Retype Password" class="bg-gray-100 border-2 w-full p-4 rounded-lg">
+
             </div>
             <div class="">
                 <button type="submit" class="bg-blue-500 rounded text-white px-4 py-3 font-medium w-full">
