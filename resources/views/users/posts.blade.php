83fe8bddcf4183da27b2ae1a85cab138531d10b6
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-09-14 21:17:39 +0530

    Single User Total Posts and Likes show done

--- resources/views/users/posts.blade.php
+++ resources/views/users/posts.blade.php
@@ -2,16 +2,26 @@
 
 @section('content')
 <div class="flex justify-center">
-    <div class="w-8/12 bg-white p-6 rounded-lg">
-        @if ($posts->count())
-        @foreach ($posts as $post)
-        <x-post :post="$post" />
-        @endforeach
-        @else
-        <div class="bg-red-400 py-3 rounded-lg text-center text-white mb-4">
-            There are no posts available
+    <div class="w-8/12">
+        <div class="p-6">
+            <h1 class="text-2xl font-medium">{{$user->name}}</h1>
+            <p>
+                Posted {{$posts->count()}} {{Str::plural('post', $posts->count())}}
+                and recieved {{$user->receivedLikes()->count()}}
+                {{Str::plural('like', $user->receivedLikes()->count())}}
+            </p>
         </div>
-        @endif
+        <div class=" bg-white p-6 rounded-lg">
+            @if ($posts->count())
+            @foreach ($posts as $post)
+            <x-post :post="$post" />
+            @endforeach
+            @else
+            <div class="bg-red-400 py-3 rounded-lg text-center text-white mb-4">
+                {{$user->name}} does not have any posts.
+            </div>
+            @endif
+        </div>
     </div>
 </div>
 @endsection
